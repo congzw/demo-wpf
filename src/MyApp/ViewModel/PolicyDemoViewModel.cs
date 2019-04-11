a28335016db3f34e5321d2c693b621dcfefe964f
congzw <2981810300@qq.com>
2019-04-11 18:29:06 +0800

    try another registry shell

--- src/MyApp/ViewModel/PolicyDemoViewModel.cs
+++ src/MyApp/ViewModel/PolicyDemoViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Libs.Common;
+﻿using System.Text;
+using Libs.Common;
 using Microsoft.Win32;
 
 namespace MyApp.ViewModel
@@ -8,6 +9,7 @@ namespace MyApp.ViewModel
         public DemoWindow DemoWindow { get; set; }
         public IGroupPolicyHelper Policy { get; set; }
         public IProcessHelper Process { get; set; }
+        public ShellHelper Shell { get; set; }
 
         public const string ShellKey = @"HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\System!Shell";
 
@@ -16,6 +18,7 @@ namespace MyApp.ViewModel
             DemoWindow = demoWindow;
             Policy = GroupPolicyHelper.Resolve();
             Process = ProcessHelper.Resolve();
+            Shell = new ShellHelper();
             demoWindow.BtnSetShell.Click += BtnSetShell_Click;
             demoWindow.BtnGetShell.Click += BtnGetShell_Click;
             demoWindow.BtnResetShell.Click += BtnResetShell_Click;
@@ -23,25 +26,64 @@ namespace MyApp.ViewModel
 
         private void BtnResetShell_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-            Policy.SetValue(ShellKey, null, RegistryValueKind.String);
-            var value = Policy.GetValue(ShellKey, "NULL");
+            Shell.Reset();
+            var value = Shell.GetValue("NULL");
             DemoWindow.ShowMessage("Reset Shell: " + value);
+
+            //Policy.SetValue(ShellKey, null, RegistryValueKind.String);
+            //var value = Policy.GetValue(ShellKey, "NULL");
+            //DemoWindow.ShowMessage("Reset Shell: " + value);
         }
 
         private void BtnGetShell_Click(object sender, System.Windows.RoutedEventArgs e)
         {
-            var value = Policy.GetValue(ShellKey, "NULL");
+            var value = Shell.GetValue("NULL");
             DemoWindow.ShowMessage("Get Shell: " + value);
+
+            //var value = Policy.GetValue(ShellKey, "NULL");
+            //DemoWindow.ShowMessage("Get Shell: " + value);
         }
 
         private void BtnSetShell_Click(object sender, System.Windows.RoutedEventArgs e)
         {
             var appPath = Process.GetCurrentExePath();
-            Policy.SetValue(ShellKey, appPath, RegistryValueKind.String);
+            Shell.SetValue(appPath);
             DemoWindow.ShowMessage("Set Shell: " + appPath);
 
-            var value = Policy.GetValue(ShellKey, "NULL");
+            var value = Shell.GetValue("NULL");
             DemoWindow.ShowMessage("Get Shell: " + value);
+
+            //var appPath = Process.GetCurrentExePath();
+            //Policy.SetValue(ShellKey, appPath, RegistryValueKind.String);
+            //DemoWindow.ShowMessage("Set Shell: " + appPath);
+
+            //var value = Policy.GetValue(ShellKey, "NULL");
+            //DemoWindow.ShowMessage("Get Shell: " + value);
+        }
+    }
+
+    public class ShellHelper
+    {
+        public ShellHelper()
+        {
+            //ShellKeyForCurrentUser = @"HKEY_Current_User\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon";
+            ShellKeyForCurrentUser = @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon";
+        }
+
+        public string ShellKeyForCurrentUser { get; set; }
+
+        public void SetValue(string shellValue)
+        {
+            Registry.SetValue(ShellKeyForCurrentUser, "Shell", shellValue);
+        }
+
+        public void Reset()
+        {
+            Registry.SetValue(ShellKeyForCurrentUser, "Shell", null);
+        }
+        public object GetValue(object defaultValue)
+        {
+            return Registry.GetValue(ShellKeyForCurrentUser, "Shell", defaultValue);
         }
     }
 }
